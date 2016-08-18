a29d220c84a3ea92d60f13934d3dcacaf168f58e
Nicholas Hallahan <nick@theoutpost.io>
2016-08-18 12:26:50 -0700

    ask user for permission to access ExternalStorage

--- app/src/main/java/io/theoutpost/helloearth/HelloMapFragment.java
+++ app/src/main/java/io/theoutpost/helloearth/HelloMapFragment.java
@@ -1,8 +1,12 @@
 package io.theoutpost.helloearth;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,11 +41,28 @@ public class HelloMapFragment extends GlobeMapFragment {
 
     @Override
     protected void controlHasStarted() {
+        Activity activity = getActivity();
+        final int REQUEST_EXTERNAL_STORAGE = 1;
+        String[] PERMISSIONS_STORAGE = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+
         File storageDir = Environment.getExternalStorageDirectory();
         File mbtilesDir = new File(storageDir, "mbtiles");
         File mbtilesFile = new File(mbtilesDir, "geography-class_medres.mbtiles");
         if (!mbtilesFile.exists()) {
-            new AlertDialog.Builder(getActivity())
+            new AlertDialog.Builder(activity)
                     .setTitle("Missing MBTiles")
                     .setMessage("Could not find MBTiles file.")
                     .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
