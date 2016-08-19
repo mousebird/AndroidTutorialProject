edf4f6f3414c79f5c4e43a3f0c79e0d64c41a866
Nicholas Hallahan <nick@theoutpost.io>
2016-08-18 17:57:29 -0700

    GeoJSON showing on map

--- app/src/main/java/io/theoutpost/helloearth/GeoJsonHttpTask.java
+++ app/src/main/java/io/theoutpost/helloearth/GeoJsonHttpTask.java
@@ -1,9 +1,15 @@
 package io.theoutpost.helloearth;
 
-
+import android.graphics.Color;
 import android.os.AsyncTask;
-
 import com.mousebird.maply.MaplyBaseController;
+import com.mousebird.maply.VectorInfo;
+import com.mousebird.maply.VectorObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class GeoJsonHttpTask extends AsyncTask<String, Void, String> {
 
@@ -14,12 +20,40 @@ public class GeoJsonHttpTask extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected String doInBackground(String... strings) {
+    protected String doInBackground(String... params) {
+        HttpURLConnection urlConnection;
+        try {
+            String urlStr = params[0];
+            URL url = new URL(urlStr);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setConnectTimeout(3000);
+            urlConnection.setReadTimeout(7000);
+            int statusCode = urlConnection.getResponseCode();
+
+            // 200 represents HTTP OK
+            if (statusCode == 200) {
+                BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = r.readLine()) != null) {
+                    response.append(line);
+                }
+                return response.toString();
+            }
+        } catch (Exception e) {
+            // didn't work
+        }
         return null;
     }
 
     @Override
-    protected void onPostExecute(String string) {
-
+    protected void onPostExecute(String json) {
+        VectorInfo vectorInfo = new VectorInfo();
+        vectorInfo.setColor(Color.RED);
+        vectorInfo.setLineWidth(4.f);
+        VectorObject object = new VectorObject();
+        if (object.fromGeoJSON(json)) {
+            controller.addVector(object, vectorInfo, MaplyBaseController.ThreadMode.ThreadAny);
+        }
     }
 }
