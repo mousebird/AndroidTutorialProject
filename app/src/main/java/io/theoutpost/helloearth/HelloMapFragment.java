b911ddbf24c0eabecf02f3b120fa9ef790fea3cf
Nicholas Hallahan <nick@theoutpost.io>
2016-08-24 16:26:58 -0700

    troubleshooting userObject

--- app/src/main/java/io/theoutpost/helloearth/HelloMapFragment.java
+++ app/src/main/java/io/theoutpost/helloearth/HelloMapFragment.java
@@ -27,6 +27,8 @@ import com.mousebird.maply.MaplyBaseController;
 import com.mousebird.maply.MarkerInfo;
 import com.mousebird.maply.Point2d;
 import com.mousebird.maply.QuadImageTileLayer;
+import com.mousebird.maply.RemoteTileInfo;
+import com.mousebird.maply.RemoteTileSource;
 import com.mousebird.maply.ScreenLabel;
 import com.mousebird.maply.ScreenMarker;
 import com.mousebird.maply.SelectedObject;
@@ -56,51 +58,51 @@ public class HelloMapFragment extends GlobeMapFragment {
 
     @Override
     protected void controlHasStarted() {
-        Activity activity = getActivity();
-        final int REQUEST_EXTERNAL_STORAGE = 1;
-        String[] PERMISSIONS_STORAGE = {
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-
-        File storageDir = Environment.getExternalStorageDirectory();
-        File mbtilesDir = new File(storageDir, "mbtiles");
-        File mbtilesFile = new File(mbtilesDir, "geography-class_medres.mbtiles");
-        if (!mbtilesFile.exists()) {
-            new AlertDialog.Builder(activity)
-                    .setTitle("Missing MBTiles")
-                    .setMessage("Could not find MBTiles file.")
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // do nothing
-                        }
-                    }).show();
-        }
-        MBTiles mbtiles = new MBTiles(mbtilesFile);
-        MBTilesImageSource localTileSource = new MBTilesImageSource(mbtiles);
+//        Activity activity = getActivity();
+//        final int REQUEST_EXTERNAL_STORAGE = 1;
+//        String[] PERMISSIONS_STORAGE = {
+//                Manifest.permission.READ_EXTERNAL_STORAGE,
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE
+//        };
+//        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//
+//        if (permission != PackageManager.PERMISSION_GRANTED) {
+//            // We don't have permission so prompt the user
+//            ActivityCompat.requestPermissions(
+//                    activity,
+//                    PERMISSIONS_STORAGE,
+//                    REQUEST_EXTERNAL_STORAGE
+//            );
+//        }
+//
+//        File storageDir = Environment.getExternalStorageDirectory();
+//        File mbtilesDir = new File(storageDir, "mbtiles");
+//        File mbtilesFile = new File(mbtilesDir, "geography-class_medres.mbtiles");
+//        if (!mbtilesFile.exists()) {
+//            new AlertDialog.Builder(activity)
+//                    .setTitle("Missing MBTiles")
+//                    .setMessage("Could not find MBTiles file.")
+//                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            // do nothing
+//                        }
+//                    }).show();
+//        }
+//        MBTiles mbtiles = new MBTiles(mbtilesFile);
+//        MBTilesImageSource localTileSource = new MBTilesImageSource(mbtiles);
 
 
         // setup base layer tiles
-//        String cacheDirName = "stamen_watercolor";
-//        File cacheDir = new File(getActivity().getCacheDir(), cacheDirName);
-//        cacheDir.mkdir();
-//        RemoteTileSource remoteTileSource = new RemoteTileSource(new RemoteTileInfo("http://tile.stamen.com/watercolor/", "png", 0, 18));
-//        remoteTileSource.setCacheDir(cacheDir);
+        String cacheDirName = "stamen_watercolor";
+        File cacheDir = new File(getActivity().getCacheDir(), cacheDirName);
+        cacheDir.mkdir();
+        RemoteTileSource remoteTileSource = new RemoteTileSource(new RemoteTileInfo("http://tile.stamen.com/watercolor/", "png", 0, 18));
+        remoteTileSource.setCacheDir(cacheDir);
         SphericalMercatorCoordSystem coordSystem = new SphericalMercatorCoordSystem();
 
         // globeControl is the controller when using MapDisplayType.Globe
         // mapControl is the controller when using MapDisplayType.Map
-        QuadImageTileLayer baseLayer = new QuadImageTileLayer(mapControl, coordSystem, localTileSource);
+        QuadImageTileLayer baseLayer = new QuadImageTileLayer(mapControl, coordSystem, remoteTileSource);
         baseLayer.setImageDepth(1);
         baseLayer.setSingleLevelLoading(false);
         baseLayer.setUseTargetZoomLevel(false);
@@ -118,10 +120,15 @@ public class HelloMapFragment extends GlobeMapFragment {
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, url);
 
         // Insert Markers
-//        insertMarkers();
+        insertMarkers();
 
         // Insert Labels
-        insertLabels();
+//        insertLabels();
+    }
+
+    private class MarkerProperties {
+        public String city;
+        public String subject;
     }
 
     /**
@@ -129,47 +136,76 @@ public class HelloMapFragment extends GlobeMapFragment {
      * https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_Russia_by_population
      */
     private void insertMarkers() {
+        // Set controller to be gesture delegate.
+        // Needed to allow selection.
+        mapControl.gestureDelegate = this;
+
         List<ScreenMarker> markers = new ArrayList<>();
 
         MarkerInfo markerInfo = new MarkerInfo();
         Bitmap icon = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.ic_city);
-        Point2d markerSize = new Point2d(144, 144);
+        Point2d markerSize = new Point2d(180, 180);
 
         // Moskow - Москва
+        MarkerProperties properties = new MarkerProperties();
+        properties.city = "Moskow";
+        properties.subject = "Moskow";
         ScreenMarker moskow = new ScreenMarker();
         moskow.loc = Point2d.FromDegrees(37.616667, 55.75); // Longitude, Latitude
         moskow.image = icon;
         moskow.size = markerSize;
+        moskow.selectable = true;
+        moskow.userObject = properties;
         markers.add(moskow);
 
-        // 	Saint Petersburg - Санкт-Петербург
-        ScreenMarker stPetersburg = new ScreenMarker();
-        stPetersburg.loc = Point2d.FromDegrees(30.3, 59.95); // Longitude, Latitude
-        stPetersburg.image = icon;
-        stPetersburg.size = markerSize;
-        markers.add(stPetersburg);
-
-        // Novosibirsk - Новосибирск
-        ScreenMarker novosibirsk = new ScreenMarker();
-        novosibirsk.loc = Point2d.FromDegrees(82.95, 55.05); // Longitude, Latitude
-        novosibirsk.image = icon;
-        novosibirsk.size = markerSize;
-        markers.add(novosibirsk);
-
-        // Yekaterinburg - Екатеринбург
-        ScreenMarker yekaterinburg = new ScreenMarker();
-        yekaterinburg.loc = Point2d.FromDegrees(60.583333, 56.833333); // Longitude, Latitude
-        yekaterinburg.image = icon;
-        yekaterinburg.size = markerSize;
-        markers.add(yekaterinburg);
-
-        // Nizhny Novgorod - Нижний Новгород
-        ScreenMarker nizhnyNovgorod = new ScreenMarker();
-        nizhnyNovgorod.loc = Point2d.FromDegrees(44.0075, 56.326944); // Longitude, Latitude
-        nizhnyNovgorod.image = icon;
-        nizhnyNovgorod.size = markerSize;
-        nizhnyNovgorod.rotation = Math.PI;
-        markers.add(nizhnyNovgorod);
+//        // 	Saint Petersburg - Санкт-Петербург
+//        properties = new MarkerProperties();
+//        properties.city = "Saint Petersburg";
+//        properties.subject = "Saint Petersburg";
+//        ScreenMarker stPetersburg = new ScreenMarker();
+//        stPetersburg.loc = Point2d.FromDegrees(30.3, 59.95); // Longitude, Latitude
+//        stPetersburg.image = icon;
+//        stPetersburg.size = markerSize;
+//        stPetersburg.selectable = true;
+//        stPetersburg.userObject = properties;
+//        markers.add(stPetersburg);
+//
+//        // Novosibirsk - Новосибирск
+//        properties = new MarkerProperties();
+//        properties.city = "Novosibirsk";
+//        properties.subject = "Novosibirsk Oblast";
+//        ScreenMarker novosibirsk = new ScreenMarker();
+//        novosibirsk.loc = Point2d.FromDegrees(82.95, 55.05); // Longitude, Latitude
+//        novosibirsk.image = icon;
+//        novosibirsk.size = markerSize;
+//        novosibirsk.selectable = true;
+//        novosibirsk.userObject = properties;
+//        markers.add(novosibirsk);
+//
+//        // Yekaterinburg - Екатеринбург
+//        properties = new MarkerProperties();
+//        properties.city = "Yekaterinburg";
+//        properties.subject = "Sverdlovsk Oblast";
+//        ScreenMarker yekaterinburg = new ScreenMarker();
+//        yekaterinburg.loc = Point2d.FromDegrees(60.583333, 56.833333); // Longitude, Latitude
+//        yekaterinburg.image = icon;
+//        yekaterinburg.size = markerSize;
+//        yekaterinburg.selectable = true;
+//        yekaterinburg.userObject = properties;
+//        markers.add(yekaterinburg);
+//
+//        // Nizhny Novgorod - Нижний Новгород
+//        properties = new MarkerProperties();
+//        properties.city = "Nizhny Novgorod";
+//        properties.subject = "Nizhny Novgorod Oblast";
+//        ScreenMarker nizhnyNovgorod = new ScreenMarker();
+//        nizhnyNovgorod.loc = Point2d.FromDegrees(44.0075, 56.326944); // Longitude, Latitude
+//        nizhnyNovgorod.image = icon;
+//        nizhnyNovgorod.size = markerSize;
+//        nizhnyNovgorod.rotation = Math.PI;
+//        nizhnyNovgorod.selectable = true;
+//        nizhnyNovgorod.userObject = properties;
+//        markers.add(nizhnyNovgorod);
 
         // Add your markers to the map controller.
         ComponentObject markersComponentObject = mapControl.addScreenMarkers(markers, markerInfo, MaplyBaseController.ThreadMode.ThreadAny);
@@ -197,7 +233,6 @@ public class HelloMapFragment extends GlobeMapFragment {
         moskow.loc = Point2d.FromDegrees(37.616667, 55.75); // Longitude, Latitude
         moskow.text = "Москва";
         moskow.layoutImportance = layoutImportance++;
-        moskow.selectable = true;
         labels.add(moskow);
 
         // 	Saint Petersburg - Санкт-Петербург
@@ -205,7 +240,6 @@ public class HelloMapFragment extends GlobeMapFragment {
         stPetersburg.loc = Point2d.FromDegrees(30.3, 59.95); // Longitude, Latitude
         stPetersburg.text = "Санкт-Петербург";
         stPetersburg.layoutImportance = layoutImportance++;
-        moskow.selectable = true;
         labels.add(stPetersburg);
 
         // Novosibirsk - Новосибирск
@@ -213,7 +247,6 @@ public class HelloMapFragment extends GlobeMapFragment {
         novosibirsk.loc = Point2d.FromDegrees(82.95, 55.05); // Longitude, Latitude
         novosibirsk.text = "Новосибирск";
         novosibirsk.layoutImportance = layoutImportance++;
-        moskow.selectable = true;
         labels.add(novosibirsk);
 
         // Yekaterinburg - Екатеринбург
@@ -221,7 +254,6 @@ public class HelloMapFragment extends GlobeMapFragment {
         yekaterinburg.loc = Point2d.FromDegrees(60.583333, 56.833333); // Longitude, Latitude
         yekaterinburg.text = "Екатеринбург";
         yekaterinburg.layoutImportance = layoutImportance++;
-        moskow.selectable = true;
         labels.add(yekaterinburg);
 
         // Nizhny Novgorod - Нижний Новгород
@@ -230,7 +262,6 @@ public class HelloMapFragment extends GlobeMapFragment {
         nizhnyNovgorod.text = "Нижний Новгород";
         nizhnyNovgorod.layoutImportance = layoutImportance++;
         nizhnyNovgorod.rotation = Math.PI / 8;
-        moskow.selectable = true;
         labels.add(nizhnyNovgorod);
 
         // Add your markers to the map controller.
